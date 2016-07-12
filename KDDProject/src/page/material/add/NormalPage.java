6af5a3759ad687a45c1cca946e529d1b631a6406
Van Anh <vananhto.bg@gmail.com>
2016-07-12 17:05:10 +0700

    Chỉnh sửa cách lấy theo position

--- KDDProject/src/page/material/add/NormalPage.java
+++ KDDProject/src/page/material/add/NormalPage.java
@@ -1,5 +1,33 @@
 package page.material.add;
 
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+
+import util.PropertiesStore;
+
+public class NormalPage extends PageEvent {
+
+	WebDriver driver;
+
+	public NormalPage(WebDriver driver) throws NumberFormatException, IOException {
+		super(driver);
+		positionOfFileNumberField = Integer
+				.parseInt(PropertiesStore.getProperty("PositionOfFileNumberField_NormalPage"));
+		positionOfAmountMaterialField = Integer
+				.parseInt(PropertiesStore.getProperty("PositionOfAmountMaterialField_NormalPage"));
+		positionOfQualityMaterialField = Integer
+				.parseInt(PropertiesStore.getProperty("PositionOfQualityMaterialField_NormalPage"));
+	
+	}
+
+
+}
+
+
+/*
+package page.material.add;
+
 import org.openqa.selenium.WebDriver;
 
 public class NormalPage extends PageEvent {
@@ -68,3 +96,4 @@ public class NormalPage extends PageEvent {
 	}
 
 }
+*/
