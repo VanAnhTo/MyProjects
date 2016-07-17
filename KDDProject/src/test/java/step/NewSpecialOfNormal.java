93e405c03a3c14b50191d33682257668d3827e9c
VanAnhTo <vananhto.bg@gmail.com>
2016-07-17 09:36:06 +0700

    Using background, scenario outline and examples

--- KDDProject/src/test/java/step/NewSpecialOfNormal.java
+++ KDDProject/src/test/java/step/NewSpecialOfNormal.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.openqa.selenium.WebDriver;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -19,64 +20,93 @@ import domain.detail.material.PageDetail;
 import scenarios.base.BaseScenario;
 
 public class NewSpecialOfNormal {
-		public WebDriver driver;
-		public LoginDetailBuilder builder;
-		protected BaseScenario baseScenario;
-		
-		public NewSpecialOfNormal() throws IOException {
-			driver = Hooks.driver;
-			builder = new LoginDetailBuilder();
-			baseScenario = new BaseScenario(driver);
-		}
+	public WebDriver driver;
+	public LoginDetailBuilder builder;
+	protected BaseScenario baseScenario;
 
-		@Given("^I open pharmacy website to add$")
-		public void i_open_pharmacy_website() throws Throwable {
-			driver.get("http://203.190.173.37:8080/kinhdoanhduoc/Pages/login.zul");
-		}
-
-		@And("^After I enter username: \"(.*)\" and password: \"(.*)\"$")
-		public void i_enter_username_and_password(String username,String password ) throws Throwable {
-			builder.withUsername(username).withPassword(password);
-		}
-
-		@And("^I login$")
-		public void i_get_homepage() throws Throwable {
-			LoginDetails loginDetails = builder.build();
-			baseScenario.user.clickLoginWith(loginDetails);
-		}
-		@When("^I go to specical of nomarl page$")
-		public void i_go_to_special_of_normal_page() throws Throwable {
-			baseScenario.user.goToAddNew2ddPage();
-		}
-		
-		PageDetailBuilder pageDetailBuilder = new PageDetailBuilder();
-		
-		@When("^I enter file number:\"(.*)\" and Sign where: \"(.*)\"$")
-		public void i_enter_file_number(String fileNumber, String signWhere) throws Throwable {
-			pageDetailBuilder.withFileNumber(fileNumber).withSignWhere(signWhere);
-		}
-		MaterialDetailBuilder materialDetailBuilder = new MaterialDetailBuilder();
-		
-		@And("^I enter material name: \"(.*)\" , ham luong:\"(.*)\", so luong: \"(.*)\", don vi tinh: \"(.*)\" TCCL: \"(.*)\"$")
-		public void i_enter_info_material(String materialName, String content, String amount, String quality, String unit ) throws Throwable {
-			materialDetailBuilder.withMaterialName(materialName).withContenMaterial(content)
-			.withAmountMaterial(amount).withQualityMateriall(quality).withUnitMaterial(unit);
-		}
-		@And("^I enter SDK cua thuoc: \"(.*)\" , ham luong cua thuoc: \"(.*)\" , dang bao che cua thuoc: \"(.*)\"$")
-		public void i_enter_import_number(String registration, String contenImport, String contrentration) throws Throwable {
-			materialDetailBuilder.withContentImportl(contenImport).withRegistrationNumber(registration).withContentration(contrentration);
-		}
-		
-		@Then("^I save infomation$")
-		public void i_save_info() throws Throwable {
-			List<MaterialDetail> materialDetailList = new ArrayList<MaterialDetail>();
-			materialDetailList.add(materialDetailBuilder.build());
-			pageDetailBuilder.withMaterialDetailList(materialDetailList);
-			PageDetail pageDetail = pageDetailBuilder.build();
-			baseScenario.user.clickAddNew2ddWith(pageDetail);
-		}
-		
-		
-		
-
+	public NewSpecialOfNormal() throws IOException {
+		driver = Hooks.driver;
+		builder = new LoginDetailBuilder();
+		baseScenario = new BaseScenario(driver);
 	}
+
+	@Given("^I open pharmacy website to add$")
+	public void i_open_pharmacy_website() throws Throwable {
+		driver.get("http://203.190.173.37:8080/kinhdoanhduoc/Pages/login.zul");
+	}
+
+	@And("^After I enter username: \"(.*)\" and password: \"(.*)\"$")
+	public void i_enter_username_and_password(String username, String password) throws Throwable {
+		builder.withUsername(username).withPassword(password);
+	}
+
+	@And("After I fill login form")
+	public void i_fill_login_form(DataTable loginDataTable) throws Throwable {
+		List<List<String>> result = loginDataTable.raw();
+		String username = result.get(1).get(0);
+		String password = result.get(1).get(1);
+		builder.withUsername(username).withPassword(password);
+	}
+
+	@And("^I login$")
+	public void i_get_homepage() throws Throwable {
+		LoginDetails loginDetails = builder.build();
+		baseScenario.user.clickLoginWith(loginDetails);
+	}
+
+	@When("^I go to specical of nomarl page$")
+	public void i_go_to_special_of_normal_page() throws Throwable {
+		baseScenario.user.goToAddNew2ddPage();
+	}
+
+	PageDetailBuilder pageDetailBuilder = new PageDetailBuilder();
+
+	@When("^I enter file number:\"(.*)\" and Sign where: \"(.*)\"$")
+	public void i_enter_file_number(String fileNumber, String signWhere) throws Throwable {
+		pageDetailBuilder.withFileNumber(fileNumber).withSignWhere(signWhere);
+	}
+
+	MaterialDetailBuilder materialDetailBuilder = new MaterialDetailBuilder();
+	
+	@And("I fill material info")
+	public void i_fill_material_info(DataTable loginDataTable) throws Throwable {
+		List<List<String>> result = loginDataTable.raw();
+		String materialName = result.get(0).get(1);
+		String content = result.get(1).get(1);
+		String amount = result.get(2).get(1);
+		String quality = result.get(3).get(1);
+		String unit = result.get(4).get(1);
+		String registration = result.get(5).get(1);
+		String contrentration = result.get(6).get(1);
+		String contenImport = result.get(7).get(1);
+
+		materialDetailBuilder.withMaterialName(materialName).withContenMaterial(content).withAmountMaterial(amount)
+		.withQualityMateriall(quality).withUnitMaterial(unit);
+		materialDetailBuilder.withContentImportl(contenImport).withRegistrationNumber(registration)
+		.withContentration(contrentration);
+	}
+
+	@And("^I enter material name: \"(.*)\" , ham luong:\"(.*)\", so luong: \"(.*)\", don vi tinh: \"(.*)\" TCCL: \"(.*)\"$")
+	public void i_enter_info_material(String materialName, String content, String amount, String quality, String unit)
+			throws Throwable {
+		materialDetailBuilder.withMaterialName(materialName).withContenMaterial(content).withAmountMaterial(amount)
+				.withQualityMateriall(quality).withUnitMaterial(unit);
+	}
+
+	@And("^I enter SDK cua thuoc: \"(.*)\" , ham luong cua thuoc: \"(.*)\" , dang bao che cua thuoc: \"(.*)\"$")
+	public void i_enter_import_number(String registration, String contenImport, String contrentration)
+			throws Throwable {
+		materialDetailBuilder.withContentImportl(contenImport).withRegistrationNumber(registration)
+				.withContentration(contrentration);
+	}
+
+	@Then("^I save infomation$")
+	public void i_save_info() throws Throwable {
+		List<MaterialDetail> materialDetailList = new ArrayList<MaterialDetail>();
+		materialDetailList.add(materialDetailBuilder.build());
+		pageDetailBuilder.withMaterialDetailList(materialDetailList);
+		PageDetail pageDetail = pageDetailBuilder.build();
+		baseScenario.user.clickAddNew2ddWith(pageDetail);
+	}
+
+}
--- KDDProject/src/test/resources/features/specialofnormal.feature
+++ KDDProject/src/test/resources/features/specialofnormal.feature
@@ -1,22 +1,28 @@
-Feature: Title of your feature
-  I want to use this template for my feature file
+Feature: Add new special of normal file
 
-  Scenario: Print title, url
+  Background: 
     Given I open pharmacy website to add
-    And After I enter username: "010203123" and password: "123"
+    And After I fill login form
+      | username  | password |
+      | 010203123 |      123 |
     And I login
+
+  Scenario Outline: Add new successfull with valid values
     When I go to specical of nomarl page
-    And I enter file number:"SDH20177" and Sign where: "Ha Noi"
-    And I enter material name: "Ten nguyen lieu thuong" , ham luong:"Ham luong, dang bao che", so luong: "20", don vi tinh: "kg" TCCL: "TCCL"
-    And I enter SDK cua thuoc: "SDK cua thuoc duoc SX" , ham luong cua thuoc: "Ham luong cua thuoc" , dang bao che cua thuoc: "dang bao che"
+    And I enter file number:"<fileNumber>" and Sign where: "<signWhere>"
+    And I fill material info
+      | materialName    | Ten nguyen lieu 11        |
+      | contenMaterial  | Ham luong, dang bao che 1 |
+      | amout           |                        20 |
+      | unit            | tan                       |
+      | TCCL            | ISO                       |
+      | registration    | SDK cua thuoc duoc SX     |
+      | contentration   | Ham luong cua thuoc       |
+      | dangbaochethuoc | dang bao che thuoc        |
     Then I save infomation
 
-  Scenario: Print title, url
-    Given I open pharmacy website to add
-    And After I enter username: "010203123" and password: "123"
-    And I login
-    When I go to specical of nomarl page
-    And I enter file number:"SDH20177" and Sign where: "Ha Noi"
-    And I enter material name: "Ten nguyen lieu thuong" , ham luong:"Ham luong, dang bao che", so luong: "20", don vi tinh: "kg" TCCL: "TCCL"
-    And I enter SDK cua thuoc: "SDK cua thuoc duoc SX" , ham luong cua thuoc: "Ham luong cua thuoc" , dang bao che cua thuoc: "dang bao che"
-    Then I save infomation
+    Examples: 
+      | fileNumber | signWhere |
+      | so1        | diachi1   |
+      | so2        | diachi2   |
+      | so3        | diachi3   |
