b604bbcddd56b965761b5b35c17b805ba30b1031
VanAnhTo <vananhto.bg@gmail.com>
2016-07-18 23:27:43 +0700

    Cap nhat jdk va cau hinh trinh duyet chrome

--- KDDProject/src/test/java/step/Hooks.java
+++ KDDProject/src/test/java/step/Hooks.java
@@ -7,44 +7,51 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
-public class Hooks{
-    public static WebDriver driver;
+public class Hooks {
+	public static WebDriver driver;
 
-    
-    @Before
-    public void openBrowser() throws MalformedURLException {
-    	System.out.println("Called openBrowser");
-    	System.setProperty("webdriver.chrome.driver",
-				"E:/setup/browerdriver/chromedriver.exe");
-    	driver = new ChromeDriver();
-    	//driver = new FirefoxDriver();
-    	driver.manage().deleteAllCookies();
-    	driver.manage().window().maximize();
-    }
+	@Before
+	public void openBrowser() throws MalformedURLException {
+		System.out.println("Called openBrowser");
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Windows/System32/config/systemprofile/.jenkins/jobs/chromedriver.exe");
+		// driver = new ChromeDriver();
 
-     
-    @After
-    public void embedScreenshot(Scenario scenario) {
-       
-        if(scenario.isFailed()) {
-        try {
-        	 scenario.write("Current Page URL is " + driver.getCurrentUrl());
-//            byte[] screenshot = getScreenshotAs(OutputType.BYTES);
-            byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-            scenario.embed(screenshot, "image/png");
-        } catch (WebDriverException somePlatformsDontSupportScreenshots) {
-            System.err.println(somePlatformsDontSupportScreenshots.getMessage());
-        }
-        
-        }
-        driver.quit();
-        
-    }
-    
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		capabilities.setCapability(ChromeOptions.CAPABILITY, options);
+		driver = new ChromeDriver(capabilities);
+
+		// driver = new FirefoxDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+	}
+
+	@After
+	public void embedScreenshot(Scenario scenario) {
+
+		if (scenario.isFailed()) {
+			try {
+				scenario.write("Current Page URL is " + driver.getCurrentUrl());
+				// byte[] screenshot = getScreenshotAs(OutputType.BYTES);
+				byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+				scenario.embed(screenshot, "image/png");
+			} catch (WebDriverException somePlatformsDontSupportScreenshots) {
+				System.err.println(somePlatformsDontSupportScreenshots.getMessage());
+			}
+
+		}
+		driver.quit();
+
+	}
+
 }
