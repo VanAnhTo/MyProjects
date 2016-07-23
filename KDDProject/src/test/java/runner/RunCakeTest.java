9f02be29c0f198d8440658de4a1b5d6b9e8c4c04
VanAnhTo <vananhto.bg@gmail.com>
2016-07-23 20:14:17 +0700

    Fixbug

--- KDDProject/src/test/java/runner/RunCakeTest.java
+++ KDDProject/src/test/java/runner/RunCakeTest.java
@@ -6,11 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features/material/",
-glue ={"step_definitions.material"},
-tags = {"~@wip", "@hoangnl", "@vananh"}, 
-format = {
-		"pretty", "html:target/cucumber", "json:target/cucumber-report.json" })
+@CucumberOptions(features = "src/test/resources/features/material/", glue = { "step_definitions.material" }, tags = {
+		"~@wip,@hoangnl,@vananh" }, format = { "pretty", "html:target/cucumber", "json:target/cucumber-report.json" })
 public class RunCakeTest {
 
 }
